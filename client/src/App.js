2717cbec9357c1267b6c4148c412a911fbc8e81a
McCormack <jason.mccormack@sage.com>
2020-01-23 13:01:17 +0000

    add navigation bar to env

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import React, { Component } from 'react';
 import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from 'react-apollo';
-import './App.css';
+import { ApolloProvider, renderToStringWithData } from 'react-apollo';
+// import './App.css';
 import Launches from './components/launches';
 import Hello from './components/hello';
 import Me from './components/me';
+import Home from './pages/home';
+import Test1 from './pages/test1';
+import MissingRoute from './pages/MissingRoute';
+import { Container } from 'react-bootstrap';
+import { NavigationBar } from './components/navigationbar';
 
-const client = new ApolloClient({
-    uri: 'http://localhost:3600/graphql',
-});
+// const client = new ApolloClient({
+//     uri: 'http://localhost:3600/graphql',
+// });
 
-function App() {
-    return (
-        <ApolloProvider client={client}>
-            <div className="container">
-                <h2>Enta Manufacturing Hackathon</h2>
-            </div>
-            <Hello />
-            <Me />
-            <Launches />
-        </ApolloProvider>
-    );
+class App extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <NavigationBar />
+                <Container>
+                    <Router>
+                        <Switch>
+                            <Route exact path="/" component={Home} />
+                            <Route exact path="/test1" component={Test1} />
+                            <Route component={MissingRoute} />
+                        </Switch>
+                    </Router>
+                </Container>
+            </React.Fragment>
+        );
+    }
 }
 
 export default App;
---
+++ client/src/components/navigationbar.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import {
+    Nav,
+    Navbar,
+    NavDropdown,
+    Button,
+    Form,
+    FormControl,
+} from 'react-bootstrap';
+import Styled from 'styled-components';
+
+const Styles = Styled.div`
+`;
+
+export const NavigationBar = () => (
+    <Styles>
+        <Navbar bg="light" expand="lg">
+            <Navbar.Brand href="/">Hackathone Environment</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                    <Nav.Link href="/">Home</Nav.Link>
+                    <Nav.Link href="/test1">Test Page</Nav.Link>
+                    <NavDropdown title="Servers" id="basic-nav-dropdown">
+                        <NavDropdown.Item href="http://localhost:3600/graphql">
+                            GraphQL Server
+                        </NavDropdown.Item>
+                        <NavDropdown.Item href="http://localhost:3700">
+                            JSON Server
+                        </NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="#action/3.4">
+                            Spare
+                        </NavDropdown.Item>
+                    </NavDropdown>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    </Styles>
+);
