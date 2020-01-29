974358a6a9fbb338997dc96095f4aac170012f1e
McCormack <jason.mccormack@sage.com>
2020-01-29 17:18:41 +0000

    refactoring

--- client/src/components/hello.js
+++ client/src/components/hello.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 import gql from 'graphql-tag';
 import { Query } from 'react-apollo';
 
 function welcome(stuff) {
+    //
+    const client = new ApolloClient({
+        uri: 'http://localhost:3600/graphql',
+    });
+
     //    const HELLO_QUERY = gql`
     //      query HelloQuery {
     //          hello
@@ -14,18 +21,20 @@ function welcome(stuff) {
     return (
         <div>
             <h5>Test Message</h5>
-            <Query query={HELLO_QUERY}>
-                {({ loading, error, data }) => {
-                    if (loading) {
-                        return <h4>Loading Data ......</h4>;
-                    }
-                    if (error) {
-                        return console.log(error);
-                    }
-                    console.log(data);
-                    return <p> {data.hello} </p>;
-                }}
-            </Query>
+            <ApolloProvider client={client}>
+                <Query query={HELLO_QUERY}>
+                    {({ loading, error, data }) => {
+                        if (loading) {
+                            return <h4>Loading Data ......</h4>;
+                        }
+                        if (error) {
+                            return console.log(error);
+                        }
+                        console.log(data);
+                        return <p> {data.hello} </p>;
+                    }}
+                </Query>
+            </ApolloProvider>
         </div>
     );
 }
--- client/src/pages/test1.js
+++ client/src/pages/test1.js
@@ -1,11 +1,5 @@
 import React from 'react';
-import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from 'react-apollo';
 import Hello from '../components/hello';
-
-const client = new ApolloClient({
-    uri: 'http://localhost:3600/graphql',
-});
 
 // an example of creating an object which
 // is then sent to a react component as a "prop"
@@ -16,15 +10,8 @@ const stuff = {
 export default function test1() {
     return (
         <div>
-            <div>
-                <h4 className="text-muted">TEST PAGE</h4>
-            </div>
-
-            <ApolloProvider client={client}>
-                <React-Fragment>
-                    <Hello {...stuff} />
-                </React-Fragment>
-            </ApolloProvider>
+            <h4 className="text-muted">TEST PAGE</h4>
+            <Hello {...stuff} />
         </div>
     );
 }
